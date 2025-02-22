ac4d909211ad08ac631e05ecc26dca5ffc0ff0f2
DanishKhan1811 <khandanish78866@gmail.com>
2025-02-22 16:48:22 +0530

    Competitors page

--- pages/competitors.vue
+++ pages/competitors.vue
@@ -1,69 +1,46 @@
 <template>
-    <div>
-        <ul class="grid grid-cols-2 gap-4 p-4 text-white">
-            <li
-                class="flex justify-center items-center gap-4 rounded-lg p-2 font-semibold border border-gray-700 bg-gray-900">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Piktochart_logo.jpg/640px-Piktochart_logo.jpg"
-                class="rounded-lg"
-                    alt="">
-                <div>
-                    <h1 class="text-xl">Competitor Name</h1>
-                    <p class="font-light text-[#b1b1b1]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, rem beatae placeat mollitia,
-                        distinctio repellendus molestias quas debitis optio assumenda fugiat dolor praesentium soluta
-                        corrupti molestiae ullam totam veniam.</p>
-                </div>
-            </li>
-            <li
-                class="flex justify-center items-center gap-4 rounded-lg p-2 font-semibold border border-gray-700 bg-gray-900">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Piktochart_logo.jpg/640px-Piktochart_logo.jpg"
-                class="rounded-lg"
-                    alt="">
-                <div>
-                    <h1 class="text-xl">Competitor Name</h1>
-                    <p class="font-light text-[#b1b1b1]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, rem beatae placeat mollitia,
-                        distinctio repellendus molestias quas debitis optio assumenda fugiat dolor praesentium soluta
-                        corrupti molestiae ullam totam veniam.</p>
-                </div>
-            </li>
-            <li
-                class="flex justify-center items-center gap-4 rounded-lg p-2 font-semibold border border-gray-700 bg-gray-900">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Piktochart_logo.jpg/640px-Piktochart_logo.jpg"
-                class="rounded-lg"
-                    alt="">
-                <div>
-                    <h1 class="text-xl">Competitor Name</h1>
-                    <p class="font-light text-[#b1b1b1]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, rem beatae placeat mollitia,
-                        distinctio repellendus molestias quas debitis optio assumenda fugiat dolor praesentium soluta
-                        corrupti molestiae ullam totam veniam.</p>
-                </div>
-            </li>
-            <li
-                class="flex justify-center items-center gap-4 rounded-lg p-2 font-semibold border border-gray-700 bg-gray-900">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Piktochart_logo.jpg/640px-Piktochart_logo.jpg"
-                class="rounded-lg"
-                    alt="">
-                <div>
-                    <h1 class="text-xl">Competitor Name</h1>
-                    <p class="font-light text-[#b1b1b1]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, rem beatae placeat mollitia,
-                        distinctio repellendus molestias quas debitis optio assumenda fugiat dolor praesentium soluta
-                        corrupti molestiae ullam totam veniam.</p>
-                </div>
-            </li>
-            <li
-                class="flex justify-center items-center gap-4 rounded-lg p-2 font-semibold border border-gray-700 bg-gray-900">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Piktochart_logo.jpg/640px-Piktochart_logo.jpg"
-                class="rounded-lg"
-                    alt="">
-                <div>
-                    <h1 class="text-xl">Competitor Name</h1>
-                    <p class="font-light text-[#b1b1b1]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, rem beatae placeat mollitia,
-                        distinctio repellendus molestias quas debitis optio assumenda fugiat dolor praesentium soluta
-                        corrupti molestiae ullam totam veniam.</p>
-                </div>
+    <div class="min-h-screen bg-gray-900 text-white p-4">
+        <h1 class="text-3xl font-bold mb-6">Competitor Analysis</h1>
+        <ul v-if="competitors.length" class="grid grid-cols-2 gap-4">
+            <li v-for="competitor in competitors" :key="competitor.id"
+                class="flex flex-col border border-gray-700 rounded-lg p-4 bg-gray-800">
+                <h2 class="text-xl font-semibold">{{ competitor.title }}</h2>
+                <p class="text-gray-400 text-sm mt-1">{{ competitor.summary }}</p>
+                <a :href="competitor.url" target="_blank" class="text-blue-400 mt-2 hover:underline">
+                    Visit Website
+                </a>
             </li>
         </ul>
+        <p v-else class="text-gray-400">Loading competitors...</p>
     </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const competitors = ref([]);
+const route = useRoute();
+
+const fetchCompetitors = async () => {
+    try {
+        const response = await axios.post('http://localhost:5000/competitor_search', {
+            keywords: route.query.keywords ? route.query.keywords.split(',') : []
+        });
+
+        competitors.value = response.data.competitors?.results.map(({ title, url, summary }) => ({
+            title,
+            url,
+            summary
+        })) || [];
+    } catch (error) {
+        console.error('Error fetching competitors:', error);
+    }
+};
+
+onMounted(fetchCompetitors);
+</script>
 
 <style>
 img {
